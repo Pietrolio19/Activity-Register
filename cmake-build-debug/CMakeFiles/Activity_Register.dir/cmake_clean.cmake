a2750d28fb0fa01678526f316a4eec7efb655571
Pietrolio19 <pietromadioni224@gmail.com>
2023-06-27 12:30:43 +0200

    wxWidgets Implemented

---
+++ cmake-build-debug/CMakeFiles/Activity_Register.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Activity_Register"
+  "Activity_Register.pdb"
+  "CMakeFiles/Activity_Register.dir/main.cpp.o"
+  "CMakeFiles/Activity_Register.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Activity_Register.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Activity_Register.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Activity_Register.
+# This may be replaced when dependencies are built.
