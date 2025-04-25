db7afd0da1cbfe77a862dadc60f2f940f1c01c9a
Pietrolio19 <pietromadioni224@gmail.com>
2025-04-25 17:01:28 +0200

    Aggiunti test per le nuove funzioni della classe Register

--- tests/Test_register.cpp
+++ tests/Test_register.cpp
@@ -9,12 +9,46 @@ TEST(RegisterTest, AddActivityTest) {
     auto time1 = Time(15, 10);
     auto time2 = Time(15, 15);
     auto act1 = std::make_shared<Activity>(text, time1, time2);
-    reg.addActivity(date, act1);
-    //migliora test
 
-    auto activities = reg.getActivities(date);
+    reg.addActivity(date, act1);
+    auto activities = reg.searchActivityByDate(date);
     ASSERT_EQ(activities.size(), 1);
-    EXPECT_EQ(activities[0]->getActivity(), act1->getActivity());
+    EXPECT_EQ(activities[0]->toString(), act1->toString());
 }
 
-//test su aggiunta, rimozione, ...
+TEST(ResisterTest, removeActivityTest) {
+    auto reg = Register();
+    auto date = Date(26,4,2025);
+    string text = "Testing!";
+    auto time1 = Time(15, 10);
+    auto time2 = Time(15, 15);
+    auto act1 = make_shared<Activity>(text, time1, time2);
+    auto act2 =make_shared<Activity>(text, time1, time2);
+    reg.addActivity(date, act1);
+    reg.addActivity(date, act2);
+
+    reg.removeActivity(date, act1);
+    auto activities = reg.searchActivityByDate(date);
+    ASSERT_EQ(activities.size(), 1);
+}
+
+TEST(RegisterTest, updateActivityTest) {
+    auto reg = Register();
+    auto date = Date(26,4,2025);
+    string text = "Testing!";
+    auto time1 = Time(15, 10);
+    auto time2 = Time(15, 15);
+
+    string text2 = "Extra Testing!";
+    auto time3 = Time(15, 15);
+    auto time4 = Time(15, 25);
+    auto act1 = make_shared<Activity>(text, time1, time2);
+    auto act2 = make_shared<Activity>(text2, time3, time4);
+
+    reg.addActivity(date, act1);
+    reg.updateActivity(date, act1, act2);
+    string expected = "Extra Testing! dalle 15:15 fino alle 15:25";
+    auto activities = reg.searchActivityByDate(date);
+    ASSERT_EQ(activities.size(), 1);
+    ASSERT_EQ(activities[0]->toString(), expected);
+}
